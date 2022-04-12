1c249fd880220ba333bbe05922b96ffbb87b66cf
Guillermo Diaz <diazgdev@gmail.com>
2022-04-12 18:41:44 -0500

    created tasks have an owner

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,17 +9,21 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  category_id :bigint           not null
+#  owner_id    :bigint           not null
 #
 # Indexes
 #
 #  index_tasks_on_category_id  (category_id)
+#  index_tasks_on_owner_id     (owner_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (category_id => categories.id)
+#  fk_rails_...  (owner_id => users.id)
 #
 class Task < ApplicationRecord
   belongs_to :category
+  belongs_to :owner, class_name: 'User'
 
   validates :name, :description, presence: true
   validates :name, uniqueness: { case_insensitive: false }
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -9,14 +9,17 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  category_id :bigint           not null
+#  owner_id    :bigint           not null
 #
 # Indexes
 #
 #  index_tasks_on_category_id  (category_id)
+#  index_tasks_on_owner_id     (owner_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (category_id => categories.id)
+#  fk_rails_...  (owner_id => users.id)
 #
 require "test_helper"
 
